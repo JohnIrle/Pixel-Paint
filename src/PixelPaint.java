92c11129935919e2bdf035a6ed9b82444ac48098
JohnIrle <irlejohn@gmail.com>
2019-03-21 13:56:59 -0500

    initial commit.

---
+++ src/PixelPaint.java
@@ -0,0 +1,14 @@
+// Pixel Paint
+// John Irle
+// 21 March 2019
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PixelPaint extends Application {
+  public void start(Stage primaryStage) {}
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
