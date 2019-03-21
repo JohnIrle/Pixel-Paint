00b0ad43af625137788648eb7f42e136d83c1f59
JohnIrle <irlejohn@gmail.com>
2019-03-21 14:31:09 -0500

    Display Empty Grid.

--- src/PixelPaint.java
+++ src/PixelPaint.java
@@ -3,10 +3,31 @@
 // 21 March 2019
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class PixelPaint extends Application {
-  public void start(Stage primaryStage) {}
+  private Pixel[][] grid = new Pixel[36][36];
+
+  public void start(Stage primaryStage) {
+    GridPane pane = new GridPane();
+
+    for (int i = 0; i < 32; i++) {
+      for (int j = 0; j < 32; j++) {
+        pane.add(grid[i][j] = new Pixel(), j, i);
+      }
+    }
+
+    BorderPane borderPane = new BorderPane();
+    borderPane.setCenter(pane);
+
+    Scene scene = new Scene(borderPane, 600,600);
+    primaryStage.setTitle("Pixel Paint");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
 
   public static void main(String[] args) {
     launch(args);
