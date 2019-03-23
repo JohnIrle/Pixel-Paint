73b4071a0e47ce5f1d7c632d0083db27fcba7a88
JohnIrle <irlejohn@gmail.com>
2019-03-23 15:08:57 -0500

    Set background of each cell on click.

--- src/Pixel.java
+++ src/Pixel.java
@@ -2,11 +2,27 @@
 // John Irle
 // 21 March 2019
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 class Pixel extends Pane {
+
+  private Color cellColor;
+
   public Pixel() {
     this.setPrefSize(2000, 2000);
     this.setStyle("-fx-border-color: black");
+    this.setOnMouseClicked(e -> handleMouseClick());
+  }
+
+  private void handleMouseClick() {
+    Color color = PixelPaint.colorPicker.getValue();
+    BackgroundFill backgroundFill = new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY);
+    this.setBackground(new Background(backgroundFill));
+    this.cellColor = color;
   }
 }
