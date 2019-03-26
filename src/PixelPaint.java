95db5382105d8907b8dc77708bfe7848a7bbc33d
JohnIrle <irlejohn@gmail.com>
2019-03-26 12:11:04 -0500

    Load colors from file and set cell backgrounds.

--- src/PixelPaint.java
+++ src/PixelPaint.java
@@ -6,15 +6,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class PixelPaint extends Application {
   private Pixel[][] grid = new Pixel[36][36];
@@ -58,7 +57,6 @@ public class PixelPaint extends Application {
       for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
           Color current = grid[i][j].getColor();
-          System.out.println(current);
           f.print((int)(current.getRed() * 255)
                   + " " + (int)(current.getGreen() * 255)
                   + " " + (int)(current.getBlue() * 255) + '\n');
@@ -71,7 +69,26 @@ public class PixelPaint extends Application {
   }
 
   private void handleLoad() {
+    File file = new File("file.txt");
+    try {
+      Scanner input = new Scanner(file);
+      for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 32; j++) {
+          int R = input.nextInt();
+          int G = input.nextInt();
+          int B = input.nextInt();
 
+          Color color = Color.rgb(R, G, B);
+
+          BackgroundFill backgroundFill = new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY);
+
+          grid[i][j].setBackground(new Background(backgroundFill));
+          grid[i][j].setColor(color);
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
